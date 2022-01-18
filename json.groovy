8e4a217235b7e96743a4486bf5a099688b6a46ff
nikt0r <lnm@interia.pl>
2022-01-18 23:51:58 +0100

    Initial commit

---
+++ json.groovy
@@ -0,0 +1,77 @@
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+
+JsonBuilder jsonBuilder = new JsonBuilder()
+
+def aaa = "bla"
+
+jsonBuilder.br {
+    parties(
+            [
+                    {
+                        id 1
+                        name aaa
+                    },
+                    {
+                        id 2
+                        name "P2"
+                        someArray "value1", "value2"
+                    }
+            ]
+    )
+    other 12
+    someObj {
+        name "Object"
+        type "Json"
+    }
+}
+
+
+println jsonBuilder.toString()
+JsonSlurper jsonSlurper = new JsonSlurper()
+slurper = jsonSlurper.parseText(jsonBuilder.toString())
+
+println slurper.br.parties[0].name
+
+JsonNode jsonNode = new ObjectMapper().readTree(jsonBuilder.toString())
+
+println jsonNode.br.parties[0].name.asText()
+
+class ToJson {
+    int id = 7
+    String name = "Extra"
+    List list = [1,2,3,4]
+}
+
+ToJson tj = new ToJson()
+
+JsonNode jn = new ObjectMapper().valueToTree(tj)
+println jn
+
+Map map = [
+        id: 7,
+        name: "buba",
+        coll: [
+                1,2,3
+        ],
+        complex: [
+                id: 1,
+                title: "this title"
+        ],
+        parties: [
+                [
+                        id: 100,
+                        name: "Party 1"
+                ],
+                [
+                        id: 101,
+                        name: "Party 2"
+                ]
+        ]
+]
+
+JsonNode node = new ObjectMapper().convertValue(map, JsonNode.class)
+
+println node
